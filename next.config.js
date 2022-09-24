78517e561cac296c339471315b8598bd456b7da4
jungle379 <jungle379@gmail.com>
2022-09-24 21:31:23 +0900

    ⚡️

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
   swcMinify: true,
-}
-
-module.exports = nextConfig
+  reactStrictMode: true,
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ["./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: {} },
   plugins: [],
 };
