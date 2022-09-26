5864c3bbafc93f7bd19f98458d6333bf54ad2542
jungle379 <jungle379@gmail.com>
2022-09-26 13:41:29 +0900

    ⚡️ add_footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="flex justify-center mb-10">
+        <Link href="/">footer_link</Link>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
@@ -8,7 +7,6 @@ const Home: NextPage = () => {
       <Head>
         <title>home</title>
       </Head>
-      <Header />
       <div className="text-blue-800 text-4xl">Hello!</div>
       <div></div>
       <div></div>
