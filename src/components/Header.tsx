bcf722351e4ba5c5d7391e0cd2b5fb4f4a03d3bc
jungle379 <jungle379@gmail.com>
2022-10-10 01:01:58 +0900

    ⚡️

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ const Header: FC = () => {
   return (
     <>
       <div className="h-[200px]">
-        <div className="my-10 text-8xl flex justify-center">"my-app"</div>
+        <div className="my-10 text-8xl flex justify-center">"review-app"</div>
       </div>
       <div className="text-4xl py-10 flex justify-center">
         <DataCounter />
