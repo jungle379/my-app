28ab0ef0abd8157efbb068c0212c6bcc010e42b0
jungle379 <jungle379@gmail.com>
2022-10-14 21:53:25 +0900

    🚑

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,9 +4,7 @@ const Footer = () => {
   return (
     <>
       <div>
-        <div className="flex justify-center my-10">
-          <Link href="/">2022©️</Link>
-        </div>
+        <div className="flex justify-center my-10">2022©️</div>
       </div>
     </>
   );
