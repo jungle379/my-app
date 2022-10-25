71e40a1d976be30e997be95c8409f17d43821f76
jungle379 <jungle379@gmail.com>
2022-10-25 17:13:47 +0900

    🔥

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -17,7 +17,7 @@ const Id: NextPage = () => {
             <label>
               {data.text}
               <br />
-              {data.day}
+              投稿日時：{data.day}
             </label>
           </div>
         ))}
@@ -26,7 +26,7 @@ const Id: NextPage = () => {
         <div className="my-10 mx-10 text-2xl border-2 border-orange-500 w-[120px] text-center">
           <Link href="/">Topへ</Link>
         </div>
-        <div className="mx-10 my-10 text-2xl text-center hover:underline w-[150px] h-[40px] border-2 border-orange-500">
+        <div className="mx-10 my-10 text-2xl text-center hover:underline w-[180px] h-[40px] border-2 border-orange-500">
           <Link href="../posts">投稿ページへ</Link>
         </div>
       </div>
