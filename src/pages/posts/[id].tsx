5c950ab265452a7992662e17978ccba11155676c
jungle379 <jungle379@gmail.com>
2022-10-27 15:33:30 +0900

    🔥

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -16,18 +16,17 @@ const Id: NextPage = () => {
         {data.map((data) => (
           <div key={data.id}>
             <label>
-              {data.text}
-              <br />
+              {data.id === 0 ? "" : <div>{data.text}</div>}
               {data.day === "" ? "" : <div>投稿日時：{data.day}</div>}
             </label>
           </div>
         ))}
       </div>
-      <div className="flex justify-center">
+      <div className="flex justify-evenly">
         <div className="my-10 mx-10 text-2xl border-2 border-orange-500 w-[120px] text-center">
           <Link href="/">ホームへ</Link>
         </div>
-        <div className="mx-10 my-10 text-2xl text-center hover:underline w-[180px] h-[40px] border-2 border-orange-500">
+        <div className="mx-10 my-10 text-2xl text-center hover:underline w-[180px] border-2 border-orange-500">
           <Link href="../posts">投稿ページへ</Link>
         </div>
       </div>
