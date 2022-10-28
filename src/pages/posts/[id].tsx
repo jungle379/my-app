d9287b1371daeeef0465da4ac24a593417f028d7
jungle379 <jungle379@gmail.com>
2022-10-28 16:07:03 +0900

    🔥

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -10,7 +10,7 @@ const Id: NextPage = () => {
   return (
     <>
       <Head>
-        <title>投稿コメント一覧</title>
+        <title>コメント一覧</title>
       </Head>
       <div>
         {data.map((data) => (
