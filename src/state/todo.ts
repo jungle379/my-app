0ebf1ae3ecc56f79134fe084c9823db30333c118
jungle379 <jungle379@gmail.com>
2022-11-01 13:50:59 +0900

    🔥

--- src/state/todo.ts
+++ src/state/todo.ts
@@ -3,8 +3,8 @@ import { Data } from "../types";
 import { selectAtom } from "jotai/utils";
 
 export const datasAtom = atom<Data[]>([
-  { id: 1, title: "1st", text: "foo", isDone: false, day: "" },
-  { id: 2, title: "2nd", text: "bar", isDone: false, day: "" },
+  { id: 1, title: "sample1", text: "foo", isDone: false, day: "" },
+  { id: 2, title: "sample2", text: "bar", isDone: false, day: "" },
 ]);
 
 export const datasLengthAtom = selectAtom(datasAtom, (datas) => datas.length);
