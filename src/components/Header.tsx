c3f830675dbd64e43fdd5eaa421023a84c8f5e3c
jungle379 <jungle379@gmail.com>
2022-11-09 13:00:06 +0900

    🔥

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,8 @@ import { FC } from "react";
 const Header: FC = () => {
   return (
     <>
-      <div className="h-[200px]">
-        <div className="my-10 text-8xl flex justify-center">"review-app"</div>
+      <div className="h-[200px] my-10 text-8xl flex justify-center">
+        "review-app"
       </div>
       <div className="text-4xl py-10 flex justify-center">
         <DataCounter />
